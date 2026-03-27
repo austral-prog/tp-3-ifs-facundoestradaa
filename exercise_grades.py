4e386229af3c58e0ffa766fd499df11a44b6ae8f
facundoestradaa <es.facundo.08@gmail.com>
2026-03-27 10:58:37 -0300

    Update exercise_grades.py

--- exercise_grades.py
+++ exercise_grades.py
@@ -20,14 +20,12 @@ def grades():
     """
 
 
-    nota = int(input())
-    limite= 0
-    limite2 = 10
+    nota= int(input())
 
-    if nota==9 or nota==10::
+    if nota==9 or nota==10:
         print("Excelente")
 
-    elif nota==7 or nota==8::
+    elif nota==7 or nota==8:
         print("Bueno")
 
     elif nota==5 or nota==6:
